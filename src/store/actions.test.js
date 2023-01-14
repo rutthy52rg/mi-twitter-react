c3e82e03f6ee5ceaf808dcf8f5f8c448479157f9
ruthGE <ruthge@MacBook-Pro-de-ruthGE.local>
2023-01-14 13:33:41 +0100

    test action tweetsLoadedSuccess

---
+++ src/store/actions.test.js
@@ -0,0 +1,15 @@
+//sincrono
+import { tweetsLoadedSuccess } from "./actions";
+import { TWEETS_LOADED_SUCCESS } from "./types";
+
+describe("tweesLoadedSuccess", () => {
+  test('should return a "TWEETS_LOADED_SUCCESS" action', () => {
+    const tweets = "tweets";
+    const expectedAction = {
+      type: TWEETS_LOADED_SUCCESS,
+      payload: tweets,
+    };
+    const action = tweetsLoadedSuccess(tweets);
+    expect(action).toEqual(expectedAction);
+  });
+});
